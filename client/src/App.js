518f4a118d0dda8ce3f2d2755282ccfe0d607770
aussftw <shagronaus@gmail.com>
2019-12-13 22:27:19 +0200

    fixing cra issues part2

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Some Shop</h1>
+      <p> for real</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import { createLogger } from "redux-logger";
+import thunk from "redux-thunk";
+
+import rootReducer from "./reducers/rootReducer";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export function configureStore(initState) {
+  const logger = createLogger();
+  const store = createStore(
+    rootReducer,
+    initState,
+    // applyMiddleware(thunk, logger),
+    composeEnhancers(applyMiddleware(thunk, logger))
+  );
+  return store;
+}
