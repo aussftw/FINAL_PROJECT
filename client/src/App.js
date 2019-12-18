9aeca33fbc7d3e20f823ee9860ba34eccc4025fc
aussftw <shagronaus@gmail.com>
2019-12-18 20:01:44 +0200

    some footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React from 'react';
+
+import Brands from './components/Brands/Brands.js';
+import Footer from './components/Footer/Footer.js';
 
 function App() {
   return (
     <div className="App">
       <h1>Some Shop</h1>
       <p> for real</p>
+      <Brands />
+      <Footer />
     </div>
   );
 }
