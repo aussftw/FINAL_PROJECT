92f9e5461a7185ac1957940d1ad643f2805e9e12
Andrey Yakovenko <yakovenkoandyua@gmail.com>
2019-12-19 17:29:42 +0200

    add new rules

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,12 +2,11 @@ import React, { useState } from "react";
 
 const Test = () => {
   const [active, setActive] = useState(false);
-  console.log(active);
+
   return (
     <div>
       <h1>Gogi</h1>
       <button
-        type="button"
         onClick={() => {
           setActive(!active);
         }}
@@ -19,3 +18,4 @@ const Test = () => {
 };
 
 export default Test;
+
