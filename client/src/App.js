6ee77045d4f83237894211404c211d7d936ba2ed
Eugen Markov <seroglaziymail@gmail.com>
2019-12-21 17:23:58 +0200

    refactoring mini-cart

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import  Test  from './components/Header';
+import React, { useState } from "react";
+import MainButton from "./components/common/buttons/MainButton";
+import CartMini from "./components/Header/CartMini/CartMini";
 
 function App() {
+  const [clicked, setClicked] = useState(false);
+
   return (
     <div className="App">
       <h1>Some Shop</h1>
       <p> for real</p>
-      <Test />
+      <MainButton text="BlaBla" onClick={() => setClicked(!clicked)} />
+      {clicked ? <CartMini /> : null}
     </div>
   );
 }
---
+++ client/src/theme.js
@@ -0,0 +1,28 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import { lightGreen, grey } from "@material-ui/core/colors";
+
+// eslint-disable-next-line no-unused-vars
+const breakePointsValues = {
+  xs: 0,
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1200,
+};
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: lightGreen[700], // main theme color
+      light: lightGreen[100], // footer bg
+      dark: lightGreen[900], // hover btn
+    },
+    secondary: {
+      main: grey[700], // main grey
+      dark: grey[900], // black
+      light: grey[200], // maybe
+    },
+  },
+});
+
+export default theme;
