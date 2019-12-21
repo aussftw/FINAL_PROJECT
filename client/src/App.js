760355ad71e02a7fda494937575d39f547d54398
YuriySuhak <58974730+YuriySuhak@users.noreply.github.com>
2019-12-21 17:32:22 +0200

    Home stock (#6)
    * home-stock-beta
    
    * home-stock-beta-2

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import React from 'react';
-
-
-
-
-import Brands from './components/Brands/Brands.js';
-import Footer from './components/Footer/index.js';
-
+import React from "react";
+import Brands from "./components/Brands/Brands";
+import Footer from "./components/Footer/index";
+import Test from "./components/Header";
+import Stock from "./components/Stock";
 
 function App() {
   return (
     <div className="App">
       <h1>Some Shop</h1>
       <p> for real</p>
-
-
-
+      <Test />
       <Brands />
       <Footer />
-
+      <Stock />
     </div>
   );
 }
--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React from "react";
 
-import theme from '../../theme';
-import useStyles from './useStyles';
-import { ThemeProvider } from '@material-ui/core/styles';
-import Hidden from '@material-ui/core/Hidden';
+import { ThemeProvider } from "@material-ui/core/styles";
+// import Hidden from '@material-ui/core/Hidden';
 import {
   Grid,
   Typography,
-  ExpansionPanel,
-  ExpansionPanelSummary,
-  ExpansionPanelDetails,
-  IconButton,
-} from '@material-ui/core';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import LocationOnSharpIcon from '@material-ui/icons/LocationOnSharp';
-import PhoneSharpIcon from '@material-ui/icons/PhoneSharp';
+  // ExpansionPanel,
+  // ExpansionPanelSummary,
+  // ExpansionPanelDetails,
+  // IconButton,
+} from "@material-ui/core";
+// import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+// import DraftsIcon from '@material-ui/icons/Drafts';
+// import LocationOnSharpIcon from '@material-ui/icons/LocationOnSharp';
+// import PhoneSharpIcon from '@material-ui/icons/PhoneSharp';
+import useStyles from "./useStyles";
+import theme from "../../theme";
 
-import ContactUs from './ContactUs/ContactUs';
-import Payment from './Payment/Payment';
-import MyAccount from './MyAccount/MyAccount';
-import Information from './Information/Information';
+import ContactUs from "./ContactUs/ContactUs";
+import Payment from "./Payment/Payment";
+import MyAccount from "./MyAccount/MyAccount";
+import Information from "./Information/Information";
+
 const Footer = () => {
   const classes = useStyles();
   // console.log(classes);
