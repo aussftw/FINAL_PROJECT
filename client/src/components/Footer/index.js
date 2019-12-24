5e5998b4315625f6cca0689ecc40ff33fae39e04
aussftw <shagronaus@gmail.com>
2019-12-24 15:17:45 +0200

    fixing merge issues

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import { Container } from '@material-ui/core';
+import React from "react";
+import { Container, Grid, Typography } from "@material-ui/core";
 
-// import { Grid } from '@material-ui/core/Hidden';
 // import { ThemeProvider } from '@material-ui/core/Hidden';
 import useStyles from "./useStyles";
 
@@ -10,7 +9,6 @@ import Payment from "./Payment/Payment";
 import MyAccount from "./MyAccount/MyAccount";
 import Information from "./Information/Information";
 
-
 import Copy from "./Copy/Copy";
 
 const Footer = () => {
@@ -18,20 +16,16 @@ const Footer = () => {
 
   return (
     <Container className={classes.bg} maxWidth>
-      <Hidden SmDown>
-        <Typography>LOGO</Typography>
-      </Hidden>
-      <Hidden MdUp>
-        <Grid container>
-          <ContactUs />
-          <Payment />
-          <MyAccount />
-          <Information />
-        </Grid>
-        <Grid>
-          <Copy />
-        </Grid>
-      </Hidden>
+      <Typography>LOGO</Typography>
+      <Grid container>
+        <ContactUs />
+        <Payment />
+        <MyAccount />
+        <Information />
+      </Grid>
+      <Grid>
+        <Copy />
+      </Grid>
     </Container>
   );
 };
--- client/src/components/ItemCard/ItemCard.js
+++ client/src/components/ItemCard/ItemCard.js
@@ -1,24 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import Box from '@material-ui/core/Box';
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import Tooltip from '@material-ui/core/Tooltip';
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import Tooltip from "@material-ui/core/Tooltip";
 
-import StarBorder from '@material-ui/icons/StarBorder';
-import Favorite from '@material-ui/icons/Favorite';
-import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+import StarBorder from "@material-ui/icons/StarBorder";
+import Favorite from "@material-ui/icons/Favorite";
+import FavoriteBorder from "@material-ui/icons/FavoriteBorder";
 
-import Rating from '@material-ui/lab/Rating';
-import useStyles from './useStyles';
+// import { Rating } from "@material-ui/lab/Rating";
+import useStyles from "./useStyles";
 
 const CardTooltipText = value => {
-  if (value === undefined) return 'Not yet rated';
+  if (value === undefined) return "Not yet rated";
   const rate = Math.round(value * 100) / 100;
 
   return `Rated ${rate} out of 5`;
@@ -30,23 +30,22 @@ const ItemCard = ({ title, value, price, inCart, inWishList }) => {
   return (
     <Card className={classes.card}>
       {inWishList ? (
-        <Tooltip title='Remove from wishlist'>
+        <Tooltip title="Remove from wishlist">
           <IconButton className={classes.wishList}>
             <Favorite />
           </IconButton>
         </Tooltip>
-        )
-        : (
-          <Tooltip title='Add to wishlist'>
-            <IconButton className={classes.wishList}>
-              <FavoriteBorder />
-            </IconButton>
-          </Tooltip>
-        )}
+      ) : (
+        <Tooltip title="Add to wishlist">
+          <IconButton className={classes.wishList}>
+            <FavoriteBorder />
+          </IconButton>
+        </Tooltip>
+      )}
       <CardActionArea
         classes={{
           root: classes.actionArea,
-          focusHighlight: classes.focusHighlight
+          focusHighlight: classes.focusHighlight,
         }}
       >
         <CardMedia
@@ -65,7 +64,7 @@ const ItemCard = ({ title, value, price, inCart, inWishList }) => {
           </Typography>
           <Tooltip title={CardTooltipText(value)}>
             <Box align="center" gutterBottom>
-              <Rating
+              {/* <Rating
                 className={classes.rating}
                 name="rating"
                 value={value}
@@ -75,21 +74,24 @@ const ItemCard = ({ title, value, price, inCart, inWishList }) => {
                 emptyIcon={
                   <StarBorder color="primary" style={{ fontSize: 18 }} />
                 }
-              />
+              /> */}
             </Box>
           </Tooltip>
           <Typography className={classes.price} align="center">
-            $
-            {price.toFixed(2)}
+            ${price.toFixed(2)}
           </Typography>
         </CardContent>
         {inCart ? (
-          <Tooltip title='Remove from cart'>
-            <Button variant="text" className={classes.removeFromCart} fullWidth>in cart</Button>
+          <Tooltip title="Remove from cart">
+            <Button variant="text" className={classes.removeFromCart} fullWidth>
+              in cart
+            </Button>
           </Tooltip>
-          )
-          :
-          <Button variant="text" fullWidth>+ add to cart</Button>}
+        ) : (
+          <Button variant="text" fullWidth>
+            + add to cart
+          </Button>
+        )}
       </CardActionArea>
     </Card>
   );
