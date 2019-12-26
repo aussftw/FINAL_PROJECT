4af9c4d6bd7226f51f6d6b8bb0aa20564e99a34a
Gleb <glebslivko777@gmail.com>
2019-12-26 20:51:16 +0200

    paper_test

--- client/src/components/Filters/index.js
+++ client/src/components/Filters/index.js
@@ -1,53 +1,129 @@
 import React from "react";
-import Box from "@material-ui/core/Box";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Grow from "@material-ui/core/Grow";
+import Paper from "@material-ui/core/Paper";
+import Popper from "@material-ui/core/Popper";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+import { makeStyles } from "@material-ui/core/styles";
 
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
+
 import ListItemText from "@material-ui/core/ListItemText";
-import Divider from "@material-ui/core/Divider";
 
-import useStyles from "./useStyles";
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+  },
+  root_paper: {
+    display: "flex",
+    "& > *": {
+      margin: theme.spacing(1),
+      width: theme.spacing(16),
+      height: theme.spacing(16),
+    },
+  },
+}));
 
-const FilterByCategory = () => {
+export default function MenuListComposition() {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+
+  const handleToggle = () => {
+    setOpen(prevOpen => !prevOpen);
+  };
+
+  const handleClose = event => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
 
   return (
-    <>
-      <Box className={classes.bg}>
-        <div>
-          <div className={classes.root} />
-          <List
-            component="nav"
-            aria-label="secondary mailbox folders"
-            className=""
-          >
-            <ListItemText primary="PRODUCT CATEGORIES" />
-            <Divider className={classes.divider} />
-            <ListItem button>
-              <ListItemText primary="Shamdock" />
-            </ListItem>
-            <Divider />
-            <ListItem button>
-              <ListItemText primary="Geranimus" />
-            </ListItem>
-            <Divider />
-            <ListItem button>
-              <ListItemText primary="Bermuda" />
-            </ListItem>
-            <Divider />
-            <ListItem button>
-              <ListItemText primary="Plants" />
-            </ListItem>
-            <Divider />
-            <ListItem button>
-              <ListItemText primary="Natural" />
-            </ListItem>
-            <Divider />
-          </List>
-        </div>
-      </Box>
-    </>
-  );
-};
+    <div className={classes.root}>
+      <div className={classes.root_paper}>
+        <Paper elevation={3}>
+          <div>
+            <List component="nav" aria-label="main mailbox folders">
+              <ListItem
+                button
+                ref={anchorRef}
+                aria-controls={open ? "menu-list-grow" : undefined}
+                aria-haspopup="true"
+                onClick={handleToggle}
+              >
+                <ListItemText primary="Shamrock" />
+              </ListItem>
+              <ListItem button>
+                <ListItemText primary="Heranimus" />
+              </ListItem>
+              <ListItem button>
+                <ListItemText primary="Bermuda" />
+              </ListItem>
+              <ListItem button>
+                <ListItemText primary="Plants" />
+              </ListItem>
+              <ListItem button>
+                <ListItemText primary="Natural" />
+              </ListItem>
+            </List>
 
-export default FilterByCategory;
+            <Popper
+              open={open}
+              anchorEl={anchorRef.current}
+              role={undefined}
+              transition
+              disablePortal
+            >
+              {({ TransitionProps, placement }) => (
+                <Grow
+                  // eslint-disable-next-line react/jsx-props-no-spreading
+                  {...TransitionProps}
+                  style={{
+                    transformOrigin:
+                      placement === "bottom" ? "center top" : "center bottom",
+                  }}
+                >
+                  <Paper>
+                    <ClickAwayListener onClickAway={handleClose}>
+                      <MenuList
+                        autoFocusItem={open}
+                        id="menu-list-grow"
+                        onKeyDown={handleListKeyDown}
+                      >
+                        <MenuItem onClick={handleClose}>Profile</MenuItem>
+                        <MenuItem onClick={handleClose}>My account</MenuItem>
+                        <MenuItem onClick={handleClose}>Logout</MenuItem>
+                      </MenuList>
+                    </ClickAwayListener>
+                  </Paper>
+                </Grow>
+              )}
+            </Popper>
+          </div>
+        </Paper>
+      </div>
+    </div>
+  );
+}
