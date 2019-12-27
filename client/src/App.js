200bf51b096665849999a610de5d0eb122cdbaab
Alina Yeganian <ps2014yeganian@gmail.com>
2019-12-27 13:11:29 +0200

    all photos of products

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,4 @@
 import React, { useState } from "react";
-// import { BrowserRouter } from "react-router-dom";
-
-import { connect } from "react-redux";
 import MainButton from "./components/common/buttons/MainButton";
 import CartMini from "./components/Header/CartMini/CartMini";
 import HomePage from "./components/HomePage/HomePage";
@@ -9,36 +6,20 @@ import Brands from "./components/Brands/Brands";
 import Header from "./components/Header";
 import SubHeader from "./components/Header/SubHeader";
 import BackToTop from "./components/common/GoUpButton";
-
 import Footer from "./components/Footer/index";
 // eslint-disable-next-line
 // import Stock from "./components/Stock";
 import Subscribe from "./components/Subscribe/Subscribe";
-import { getCategories } from "./store/actions";
 
-function App(props) {
+function App() {
   const [clicked, setClicked] = useState(false);
-  // useEffect(
-  //   () => {
-  //     props.getCategories();
-  //     // return props.category.categories;
-  //   },[props],
-  // );
 
   return (
     <div className="App">
       <BackToTop />
       <Header />
-
-      <HomePage />
-      <button onClick={() => props.getCategories()}>Categories</button>
-      {/* <p> */}
-      {/*  { console.log(props.category.categories)} */}
-      {/* </p> */}
-
       <SubHeader />
-      {/* categories={props.category.categories} */}
-
+      <HomePage />
       <MainButton text="BlaBla" onClick={() => setClicked(!clicked)} />
       {clicked ? <CartMini /> : null}
       {/* eslint-disable-next-line */}
@@ -50,17 +31,4 @@ function App(props) {
   );
 }
 
-function mapStateToProps(state) {
-  return {
-    category: state.categoriesReducer,
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    getCategories: () => dispatch(getCategories()),
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
-// export default App;
+export default App;
--- client/src/components/Header/SubHeader.js
+++ client/src/components/Header/SubHeader.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import axios from "axios";
 
 import { makeStyles } from "@material-ui/core";
 
@@ -27,26 +26,6 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-// function post() {
-//
-//   const Succulents = [
-//
-//   ];
-//   Succulents.forEach(function(item) {
-//     axios
-//       .post("/products",item,{
-//              headers: {
-//                Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlMDNjOTJmMTlmMjIzMDM1MDFhM2Q1MiIsImZpcnN0TmFtZSI6IkFsaW5hIiwibGFzdE5hbWUiOiJZZWdhbmlhbiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTU3NzM3NzU1NSwiZXhwIjoxNTc3NDEzNTU1fQ.v9hONNr7dhx4rV57CZXznFRoziJZVuU24KoJ4Zh51vo",
-//              } })
-//       .then(newProduct => {
-//         console.log(newProduct);
-//       })
-//       .catch(err => {
-//         console.log(err);
-//       });
-//
-//   });
-
 export default function SubHeader(props) {
   const { categories } = props;
   const classes = useStyles();
@@ -64,7 +43,6 @@ export default function SubHeader(props) {
   return (
     <Box display="flex" justifyContent="center" className={classes.subheader}>
       <Box>
-        {/* <button onClick={post}>22</button> */}
         <Link className={classes.link} to="/">
           Home
         </Link>
