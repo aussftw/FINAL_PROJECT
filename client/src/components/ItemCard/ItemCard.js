a160f126797c81193f2caa3f7d8970a8b9ca9bfb
AlexandrSun <58847973+AlexandrSun@users.noreply.github.com>
2019-12-27 16:51:29 +0200

    Update ItemCard.js

--- client/src/components/ItemCard/ItemCard.js
+++ client/src/components/ItemCard/ItemCard.js
@@ -10,7 +10,7 @@ import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import Tooltip from "@material-ui/core/Tooltip";
 
-// import StarBorder from "@material-ui/icons/StarBorder";
+import StarBorder from "@material-ui/icons/StarBorder";
 import Favorite from "@material-ui/icons/Favorite";
 import FavoriteBorder from "@material-ui/icons/FavoriteBorder";
 
