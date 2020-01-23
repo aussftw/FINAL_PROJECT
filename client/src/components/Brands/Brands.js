772afa3622b783e449957420a867cd8f3ccf9d73
aussftw <shagronaus@gmail.com>
2020-01-23 19:30:13 +0200

    brands_fixed_tablet_bug

--- client/src/components/Brands/Brands.js
+++ client/src/components/Brands/Brands.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 
 import axios from "axios";
 
-// import { Gallery, GalleryImage } from "react-gesture-gallery";
-
 import { Container, Link, Box } from "@material-ui/core";
 import PreloaderAdaptive from "../Preloader/Adaptive";
 import useStyles from "./useStyles";
@@ -16,7 +14,7 @@ const Brands = () => {
 
   useEffect(() => {
     axios
-      .get("/api/partners")
+      .get("/partners")
       .then(response => {
         setBrands(response.data);
         setPreloader(false);
@@ -28,7 +26,7 @@ const Brands = () => {
   }, []);
 
   return (
-    <Container className={classes.brandsContaier}>
+    <Container className={classes.brandsContaier} maxWidth="xl">
       {brands.length === 0 ? (
         <PreloaderAdaptive />
       ) : (
