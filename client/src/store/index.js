005d5f4ddbe76a7c39761a5535a3f6dc9f8a9ac7
Eugen Markov <seroglaziymail@gmail.com>
2020-01-24 02:28:16 +0200

    code refactoring after fiedback

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import { persistStore } from "redux-persist";
-// eslint-disable-next-line
 import { createLogger } from "redux-logger";
 import thunk from "redux-thunk";
 
