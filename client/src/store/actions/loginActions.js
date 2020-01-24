6c4ce5226fdefe5aedc5b67104ea794ba72533f3
ZhiOne <58882048+ZhiOne@users.noreply.github.com>
2020-01-24 20:37:53 +0200

    Go back (#67)
    * Registration without validation and request
    
    * save changes
    
    * CardTest (#14)
    
    * fixing merge issues
    
    *  fixing lock
    
    * working with lock
    
    * fixing clinet
    
    * Headers (#17)
    
    * add favicon
    
    * theme with colors (#3)
    
    * first header version:
    
    * two headers and burger panel
    
    * header test commit
    
    * up button
    
    * fixing GoUpBtn
    
    * hardcode objects for categories
    
    * product obj hardcode
    
    * products img adding& redux reducer
    
    * all photos of products
    
    * product imgs adding
    
    * imgs to dev
    
    Co-authored-by: yakovenkoandyUA <54232035+yakovenkoandyUA@users.noreply.github.com>
    Co-authored-by: ZhiOne <58882048+ZhiOne@users.noreply.github.com>
    
    * Home stock (#18)
    
    * +images.js
    
    * +useStyle+const.js
    
    * Card (#19)
    
    * CardTest
    
    * Card server response
    
    * Update ItemCard.js
    
    * Card (#20)
    
    * CardTest
    
    * Card server response
    
    * add images urls
    
    * Add url to images in cards
    
    * Add images urls to cards
    
    * Sub logic (#21)
    
    * boilerplate for functions
    
    * setEmailHook updated
    
    * post-req_mvp_finished
    
    * cleaning subscribe
    
    * cleaning subscribe
    
    * Update keys_dev.js
    
    * styling footer
    
    * footer styles
    
    * some styles
    
    * partners added to bd
    
    * brands now can fetch imgs form db
    
    * qty counter added
    
    * itemDetatils boilerplate
    
    * styling itemDetails
    
    * working on itemdDetails style
    
    *  working on item-details
    
    * slider added
    
    * some adpatations with brands, little bit of cleaning
    
    * some upgrades
    
    * some upgrades
    
    * footer refactoring
    
    * footer refactored
    
    * footer refactored
    
    * item details conditional rendering on button added
    
    * hotfix beforem dev merge
    
    * card update
    
    * Footer styles (#23)
    
    * styling footer
    
    * footer styles
    
    * some styles
    
    * partners added to bd
    
    * brands now can fetch imgs form db
    
    * Refactor file structure
    
    * save changes
    
    * Login form and Registration form with redirect + routs added
    
    * registration and login with axios and refactoring
    
    * local merge dev to MAX_Login and some rewievs
    
    * Validation with error and redirect to Login adter registr
    
    * Registartion Complete + Login semi Done
    
    * re-commit
    
    * re-commit 14/01
    
    * authentication and etc and etc
    
    * add preloader for main pafr before auth
    
    * login redux all
    
    * login redux all
    
    * preloader login with redux
    
    * preloader login with redux
    
    * set error msg login form redux
    
    * set error msg login form redux
    
    * GO back just try
    
    * goBack ready
    
    * go back ready ++
    
    Co-authored-by: AlexandrSun <58847973+AlexandrSun@users.noreply.github.com>
    Co-authored-by: aussftw <34726947+aussftw@users.noreply.github.com>
    Co-authored-by: ayeganian <52545883+ayeganian@users.noreply.github.com>
    Co-authored-by: yakovenkoandyUA <54232035+yakovenkoandyUA@users.noreply.github.com>
    Co-authored-by: YuriySuhak <58974730+YuriySuhak@users.noreply.github.com>

--- client/src/store/actions/loginActions.js
+++ client/src/store/actions/loginActions.js
@@ -36,9 +36,11 @@ export const getUser = () => dispatch => {
   axios
     .get("/api/customers/customer")
     .then(response => {
-      if (response.statusText === "OK") {
+      if (response.statusText === "OK" && response.data.success) {
+        dispatch(userFromJwt(jwt(response.data.token)));
         // eslint-disable-next-line no-undef
         localStorage.setItem("user", JSON.stringify(response.data));
+
       }
       dispatch(logInSuccess(response.data));
     })
@@ -61,8 +63,8 @@ export const logIn = user => dispatch => {
         setAuthToken(response.data.token);
         dispatch(userFromJwt(jwt(response.data.token)));
       }
-      dispatch(getUser());
       dispatch(getWishlist());
+      dispatch(getUser());
       dispatch(mergeCarts());
     })
     .catch(error => {
