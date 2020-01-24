fc14c9b8d459888c7254f1fa4b332bd3b16bdd5d
ayeganian <52545883+ayeganian@users.noreply.github.com>
2020-01-25 01:02:23 +0200

    Header bugs (#68)
    * add favicon
    
    * theme with colors (#3)
    
    * first header version:
    
    * two headers and burger panel
    
    * header test commit
    
    * up button
    
    * fixing GoUpBtn
    
    * hardcode objects for categories
    
    * product obj hardcode
    
    * products img adding& redux reducer
    
    * all photos of products
    
    * product imgs adding
    
    * imgs to dev
    
    * fixed burger menu
    
    * fixed login btn
    
    * mini cart toggling
    
    * new img add
    
    * changes in server.js
    
    * fixing wishlist icon
    
    * delete duplicated files
    
    * search fixed & actions in files
    
    * fixed search&burgerMenu bugs, add filters
    
    Co-authored-by: yakovenkoandyUA <54232035+yakovenkoandyUA@users.noreply.github.com>
    Co-authored-by: ZhiOne <58882048+ZhiOne@users.noreply.github.com>

--- client/src/pages/Shop/Shop.js
+++ client/src/pages/Shop/Shop.js
@@ -2,22 +2,23 @@ import React from "react";
 import Header from "../../components/Header";
 import SubHeader from "../../components/Header/SubHeader/SubHeader";
 import BackToTop from "../../components/common/GoUpButton";
+import Filters from "../../components/Filters";
 import Footer from "../../components/Footer";
 import Subscribe from "../../components/Subscribe/Subscribe";
-import AboutUsPage from "../AboutUsPage/AboutUsPage";
-import Products from "../../components/Filters";
 
 function Shop() {
+
   return (
-    <>
-      <BackToTop />
-      <Header />
-      <Products />
-      <SubHeader />
-      <AboutUsPage />
-      <Subscribe />
-      <Footer />
-    </>
+    <div>
+      <>
+        <BackToTop />
+        <Header />
+        <SubHeader />
+        <Filters />
+        <Subscribe />
+        <Footer /> 
+      </>
+    </div>
   );
 }
 
