6cd93024ea1652903b21b653eca63bbc5465cecc
Gleb <glebslivko777@gmail.com>
2020-01-27 20:00:30 +0200

    Carousel

--- client/src/components/ProductCarousel/index.js
+++ client/src/components/ProductCarousel/index.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
 import * as axios from "axios";
 
-import AliceCarousel from 'react-alice-carousel'
-import 'react-alice-carousel/lib/alice-carousel.css'
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 
 import Container from "@material-ui/core/Container";
 import ItemCard from "../ItemCard/ItemCard";
@@ -20,6 +20,7 @@ const ProductCarousel = () => {
       .get("/api/products/top")
       .then(response => {
         setProductsV(response.data);
+        console.log("CAROUSEL", response.data);
       })
       .catch(err => {
         // eslint-disable-next-line no-console
@@ -31,42 +32,41 @@ const ProductCarousel = () => {
     <Container maxWidth="lg">
       <h2 className={classes.title}>Our TOP products</h2>
       {!productsV ? (
-          <Preloader />
+        <Preloader />
       ) : (
-          <AliceCarousel
-              responsive={
-               { 960: {
-                items: 2,
-              },
-                1280: {
-                items: 4
-              },
-              1960: {
-                 items: 4
-              }
-               }
-              }
-              buttonsDisabled={true}
-              autoPlay={true}
-              autoPlayInterval={1800}
-              duration={600}
-          >
-            {productsV.slice(0, 8).map(value => {
-              return (
-                  <ItemCard
-                      id={value._id}
-                      itemNo={value.itemNo}
-                      title={value.name}
-                      rate={value.rate.rating}
-                      price={value.currentPrice}
-                      img={value.imageUrls[0]}
-                      stock={value.quantity}
-                  />
-              );
-            })}
-          </AliceCarousel>
+        <AliceCarousel
+          responsive={{
+            960: {
+              items: 2,
+            },
+            1280: {
+              items: 4,
+            },
+            1960: {
+              items: 4,
+            },
+          }}
+          buttonsDisabled={true}
+          autoPlay={true}
+          autoPlayInterval={1800}
+          duration={600}
+        >
+          {productsV.slice(0, 8).map(value => {
+            return (
+              <ItemCard
+                id={value._id}
+                itemNo={value.itemNo}
+                title={value.name}
+                rate={value.rate.rating}
+                price={value.currentPrice}
+                img={value.imageUrls[0]}
+                stock={value.quantity}
+              />
+            );
+          })}
+        </AliceCarousel>
       )}
-       </Container>
+    </Container>
   );
 };
 
