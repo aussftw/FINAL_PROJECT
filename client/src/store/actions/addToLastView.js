b9f6f2b049d3e40c5ea83fd6b4ad7ffb4baed35f
Gleb <glebslivko777@gmail.com>
2020-01-29 17:59:49 +0200

    new action logic

--- client/src/store/actions/addToLastView.js
+++ client/src/store/actions/addToLastView.js
@@ -1,8 +1,36 @@
 import * as constants from "../constants";
+import { store } from "../index";
 
-export const addToLastView = data => {
+export const addToLastViewSuccess = data => {
   return {
-    type: constants.ADD_TO_LAST_VIEW,
+    type: constants.ADD_TO_LAST_VIEW_SUCCESS,
     payload: data,
   };
+};
+
+export const addToLastView = data => dispatch => {
+  let query = [];
+
+  if (store.getState().lastViewReducer.lastView.length < 1) {
+    query.push(data);
+    dispatch(addToLastViewSuccess(query));
+  } else {
+    query = store.getState().lastViewReducer.lastView.map(item => {
+      if (item !== data) {
+        return item;
+      }
+      console.log("query", query);
+      return false;
+    });
+
+    query.unshift(data);
+
+    let newQuery = [];
+    if (query.length > 4) {
+      newQuery = query.slice(0, 3);
+    }
+    console.log("newQuery", newQuery);
+
+    dispatch(addToLastViewSuccess(newQuery));
+  }
 };
