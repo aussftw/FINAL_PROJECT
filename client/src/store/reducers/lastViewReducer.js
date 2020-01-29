68b07550fc0329338f7a843276864d97b03f69a7
Yuriy Suhak <iri13ua@gmail.com>
2020-01-29 22:53:46 +0200

    crash

--- client/src/store/reducers/lastViewReducer.js
+++ client/src/store/reducers/lastViewReducer.js
@@ -2,12 +2,13 @@ import * as constants from "../constants";
 
 const initialState = {
   lastView: [],
+  error: ""
 };
 
 function lastViewReducer(state = initialState, action) {
   switch (action.type) {
-    case constants.ADD_TO_LAST_VIEW:
-      return { ...state, lastView: state.lastView.concat(action.payload) };
+    case constants.ADD_TO_LAST_VIEW_SUCCESS:
+      return { ...state, lastView: action.payload, error: "" };
 
     default:
       return state;
